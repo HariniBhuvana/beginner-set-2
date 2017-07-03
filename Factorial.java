bdb5404ae9b11ebf32645ba5c8840c405140b903
Harini <harinee2015@gmail.com>
2017-07-03 22:33:07 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,8 +2,9 @@
     import java.util.*;
     class Factorial{  
      public static void main(String args[]){  
-      int i,fact=1;  
-      int number=5;//It is the number to calculate factorial    
+      int i,fact=1; 
+         Scanner s=new Scanner(System.in);
+      int number=s.nextInt();//It is the number to calculate factorial    
       for(i=1;i<=number;i++){    
           fact=fact*i;    
       }    
